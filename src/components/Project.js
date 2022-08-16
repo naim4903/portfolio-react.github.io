029f053fb69865b03dfa47209f4b79450abea57f
Naim Tarafder <naim4903@gmail.com>
2022-08-16 19:58:43 +0530

    images

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,12 +10,12 @@ const Project = () => {
                         <h1 className="text-center">My Projects</h1>
                     </div>
                     <div className="row mt-4">
-                        <Card img='https://source.unsplash.com/29JBoNy1rhE' project='Counter App' />
-                        <Card img='https://source.unsplash.com/t_7NDCsxKFI' project='Tic-Tac-toe' />
-                        <Card img='https://source.unsplash.com/cLTHKmQS0zI' project='Crud Operation' />
+                        <Card img='/images/counter.png' project='Counter App' />
+                        <Card img='/images/theme-toggler.png' project='Theme Toggler' />
+                        <Card img='/images/Tic-Tac-Toe.png' project='Tic Tac Toe' />
                     </div>
                     <div className="row mt-4">
-                        <Card img='https://source.unsplash.com/KDBWMSq9wh0' project='Tours' />
+                        <Card img='images/random user.png' project='Random User' />
                         <Card img='https://source.unsplash.com/Jqa53u4Q2g4' project='Grocery Bud' />
                         <Card img='https://source.unsplash.com/W87RB3CmL3o' project='Shopping Cart' />
                     </div>
